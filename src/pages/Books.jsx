38527c5d382085898f4d4f4f4db5769f16606b46
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-16 09:21:17 +0300

    working with sql

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,7 +1,33 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import axios from 'axios'
 
 function Books (){
+
+    const [books, setBooks] = useState([])
+
+    useEffect(()=>{
+
+        const fetchBooks = async ()=>{
+
+            try{
+
+                const response = await axios.get('http://localhost:3003/allbooks')
+                // console.log(response)
+                setBooks(response.data)
+
+
+            }
+            catch(err){
+                console.log(err)
+            }
+
+
+        }
+
+        fetchBooks()
+
+
+    },[])
 
     return(
         <>
