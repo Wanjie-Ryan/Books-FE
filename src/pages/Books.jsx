2980b8df47a3b777a909d70ea9a1d457deb73ffb
Ryan <ryanwanjiedistortion@gmail.com>
2023-09-16 09:30:42 +0300

    working with sql

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,53 +1,41 @@
-import React,{useState,useEffect} from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+function Books() {
+  const [books, setBooks] = useState([]);
 
-function Books (){
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await axios.get("http://localhost:3003/allbooks");
+        // console.log(response)
+        setBooks(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
 
-    const [books, setBooks] = useState([])
+    fetchBooks();
+  }, []);
 
-    useEffect(()=>{
+  return (
+    <>
+      <h3>Wanjie Books Shop</h3>
 
-        const fetchBooks = async ()=>{
-
-            try{
-
-                const response = await axios.get('http://localhost:3003/allbooks')
-                // console.log(response)
-                setBooks(response.data)
-
-
-            }
-            catch(err){
-                console.log(err)
-            }
-
-
-        }
-
-        fetchBooks()
-
-
-    },[])
-
-    return(
-        <>
-
-            <h3>Wanjie Books Shop</h3>
-
-            <div className='books'>
-                {books.map((item)=>(
-                    <div className='book' key={item.id}>
-                        {item.cover && <img src={item.cover} alt='book-image'/>}
-                        <h2>{item.title}</h2>
-                        <p>{item.desc}</p>
-                    </div>
-
-                ))}
-            </div>
-            <button>Add New Book</button>
-        
-        </>
-    )
+      <div className="books">
+        {books.map((item) => (
+          <div className="book" key={item.id}>
+            {item.cover && <img src={item.cover} alt="book-image" />}
+            <h2>{item.title}</h2>
+            <p>{item.desc}</p>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add New Book</Link>
+      </button>
+    </>
+  );
 }
 
-export default Books
+export default Books;
